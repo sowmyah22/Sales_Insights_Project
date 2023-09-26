5f38ded10f03a287005b8a5bbc890e6442eb1317
Soumya <sowmyah871@gmail.com>
2023-09-26 22:23:47 +0400

    Delete Sales_Insights directory

--- Sales_Insights/sales_data_analysis.sql
+++
@@ -1,2 +0,0 @@
-select * from sales.markets;
-select distinct transactions.product_code,markets.markets_name from transactions inner join markets on transactions.market_code=markets.markets_code where transactions.product_code in ("Prod040","Prod159","Prod065","Prod018","Prod053") order by transactions.product_code asc; 
